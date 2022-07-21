eeb19f259f71caac1d726f2443ab6187d636ca19
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2022-07-21 18:57:37 -0300

    fetch agregado

---
+++ js/cartMain.js
@@ -0,0 +1,69 @@
+let users;
+let cart;
+let products;
+// localStorage.clear()
+
+if(JSON.parse(localStorage.getItem('cart')) || JSON.parse(localStorage.getItem('users')))  {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    users = JSON.parse(localStorage.getItem('users'))
+} else {
+    localStorage.setItem('cart', JSON.stringify([]))
+    localStorage.setItem('users', JSON.stringify([]))
+    cart = JSON.parse(localStorage.getItem('cart'))
+    users = JSON.parse(localStorage.getItem('users'))
+}
+fetchData();
+function fetchData(){
+    fetch('json/products.json')
+    .then(function(res){
+        return res.json();
+    })
+    .then((data) => {
+        Products = data;
+        let card ='';
+        data.forEach(function(product){
+            card += `
+            <li class="card">
+                <img src=${product.img} class="imgProducts" alt="${product.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">$${product.price}</p>
+                </div>
+                <div class="comprar">
+                    <button id="${product.id}" class="btn btn-dark agregar-carrito">Agregar al Carrito</button>
+                </div>
+            </li>
+            `
+        });
+        const containerProduct = document.getElementById('containerProduct')
+        containerProduct.innerHTML = card
+    })
+    .catch(function(error){
+        document.write("Error en los servidores")
+    })
+}
+
+document.addEventListener("click", (e)=>{
+    if(e.target && e.target.matches("button.btn")){
+        addCart(e.target.id);
+    }
+})
+
+function addCart(e){
+    const idFound = Products.find(prod => prod.id == e)
+    const inCart = cart.find(prod => prod.id == idFound.id)
+    if(!inCart) {
+        cart.push({...idFound, cantidad: 1})
+    } else {
+        let cartFilter = cart.filter(id => id.id != inCart.id)
+        cart = [...cartFilter, {...inCart, cantidad: inCart.cantidad + 1}]
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    swal({
+        title: `${idFound.name}`,
+        text: "has añadido este item tu carrito",
+        icon: "success",
+        timer: 6000,
+    });
+}
+
